577f8f1546aaf10ee2ce4af5d74f45834c407c9c
Fedor <fedor.musatov@yandex.ru>
2025-12-22 14:51:07 +0400

    fixed const variable

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let items = [
-	"Сделать проектную работу",
-	"Полить цветы",
-	"Пройти туториал по Реакту",
-	"Сделать фронт для своего проекта",
-	"Прогуляться по улице в солнечный день",
-	"Помыть посуду",
+const defaultItems = [
+    "Сделать проектную работу",
+    "Полить цветы",
+    "Пройти туториал по Реакту",
+    "Сделать фронт для своего проекта",
+    "Прогуляться по улице в солнечный день",
+    "Помыть посуду",
 ];
 
 const listElement = document.querySelector(".to-do__list");
 const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
+let currentTasks;
+
 function loadTasks() {
     const savedTasks = localStorage.getItem('tasks');
     if (savedTasks) {
         return JSON.parse(savedTasks);
     } else {
-        return items;
+        return defaultItems;
     }
 }
 
@@ -36,22 +38,22 @@ function createItem(item) {
 
     textElement.addEventListener('blur', function () {
         textElement.setAttribute('contenteditable', 'false');
-        const currentTasks = getTasksFromDOM();
-        saveTasks(currentTasks);
+        const currentTasksFromDOM = getTasksFromDOM();
+        saveTasks(currentTasksFromDOM);
     });
 
     deleteButton.addEventListener('click', function () {
         clone.remove();
-        const currentTasks = getTasksFromDOM();
-        saveTasks(currentTasks);
+        const currentTasksFromDOM = getTasksFromDOM();
+        saveTasks(currentTasksFromDOM);
     });
 
     duplicateButton.addEventListener('click', function () {
         const currentTaskText = textElement.textContent;
         const newTaskElement = createItem(currentTaskText);
         listElement.prepend(newTaskElement);
-        const currentTasks = getTasksFromDOM();
-        saveTasks(currentTasks);
+        const currentTasksFromDOM = getTasksFromDOM();
+        saveTasks(currentTasksFromDOM);
     });
     return clone;
 }
@@ -70,9 +72,9 @@ function saveTasks(tasks) {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-items = loadTasks();
+currentTasks = loadTasks();
 
-items.forEach(item => {
+currentTasks.forEach(item => {
     const taskElement = createItem(item);
     listElement.append(taskElement);
 });
@@ -83,8 +85,8 @@ formElement.addEventListener('submit', function (event) {
     if (taskText !== '') {
         const newTaskElement = createItem(taskText);
         listElement.prepend(newTaskElement);
-        items = getTasksFromDOM();
-        saveTasks(items);
+        currentTasks = getTasksFromDOM();
+        saveTasks(currentTasks);
         formElement.reset();
     }
 });
